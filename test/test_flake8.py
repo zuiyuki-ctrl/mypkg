a8f15b0cd2889b9546e07f4caf895bbf35ff8de9
zuiyuki-ctrl <wo20050526@qq.com>
2026-01-18 06:57:31 +0000

    Fix import order in test files for isort compliance

--- test/test_flake8.py
+++ test/test_flake8.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
-
 from ament_flake8.main import main_with_errors
+
+import pytest
 
 
 @pytest.mark.flake8
--- test/test_pep257.py
+++ test/test_pep257.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
-
 from ament_pep257.main import main
+
+import pytest
 
 
 @pytest.mark.linter
